bc86103e8cfbf4d659a7e92406b3ad24dc9b1fb3
Dylan-Speight <d.speight92@gmail.com>
2019-05-03 12:25:19 +1000

    Fixing schema

--- db/schema.rb
+++ db/schema.rb
@@ -15,10 +15,6 @@ ActiveRecord::Schema.define(version: 2019_05_03_014317) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "store_names", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
