f68dbcbe533166ecb2a51a58cda56f5598812cc0
Jack Walker <jwrc@protonmail.com>
2019-05-09 14:33:47 +1000

    Added product seed for heroku deployment

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.create(name: 'Product 1', description: "Description 1", price: "#{rand(0..50)}", category: "Cat 1")
+Product.create(name: 'Product 2', description: "Description 2", price: "#{rand(0..50)}", category: "Cat 2")
+Product.create(name: 'Product 3', description: "Description 3", price: "#{rand(0..50)}", category: "Cat 3")
+Product.create(name: 'Product 4', description: "Description 4", price: "#{rand(0..50)}", category: "Cat 4")
+Product.create(name: 'Product 5', description: "Description 5", price: "#{rand(0..50)}", category: "Cat 5")
+Product.create(name: 'Product 6', description: "Description 6", price: "#{rand(0..50)}", category: "Cat 6")
+Product.create(name: 'Product 7', description: "Description 7", price: "#{rand(0..50)}", category: "Cat 7")
+Product.create(name: 'Product 8', description: "Description 8", price: "#{rand(0..50)}", category: "Cat 8")
+Product.create(name: 'Product 9', description: "Description 9", price: "#{rand(0..50)}", category: "Cat 9")
+Product.create(name: 'Product 10', description: "Description 10", price: "#{rand(0..50)}", category: "Cat 10")
