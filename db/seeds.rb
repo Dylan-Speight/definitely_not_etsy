320fcb3f6075b58ac438d93b5aa062bce07cca9e
Jack Walker <jwrc@protonmail.com>
2019-05-09 14:48:51 +1000

    Added more to database seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,13 +6,17 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Product.create(name: 'Product 1', description: "Description 1", price: "#{rand(0..50)}", category: "Cat 1")
-Product.create(name: 'Product 2', description: "Description 2", price: "#{rand(0..50)}", category: "Cat 2")
-Product.create(name: 'Product 3', description: "Description 3", price: "#{rand(0..50)}", category: "Cat 3")
-Product.create(name: 'Product 4', description: "Description 4", price: "#{rand(0..50)}", category: "Cat 4")
-Product.create(name: 'Product 5', description: "Description 5", price: "#{rand(0..50)}", category: "Cat 5")
-Product.create(name: 'Product 6', description: "Description 6", price: "#{rand(0..50)}", category: "Cat 6")
-Product.create(name: 'Product 7', description: "Description 7", price: "#{rand(0..50)}", category: "Cat 7")
-Product.create(name: 'Product 8', description: "Description 8", price: "#{rand(0..50)}", category: "Cat 8")
-Product.create(name: 'Product 9', description: "Description 9", price: "#{rand(0..50)}", category: "Cat 9")
-Product.create(name: 'Product 10', description: "Description 10", price: "#{rand(0..50)}", category: "Cat 10")
+
+
+Store.create(store_name: "Store 1")
+
+Product.create(name: 'Product 1', description: "Description 1", price: "#{rand(0..50)}", category: "Cat 1", store_id: 1)
+Product.create(name: 'Product 2', description: "Description 2", price: "#{rand(0..50)}", category: "Cat 2", store_id: 1)
+Product.create(name: 'Product 3', description: "Description 3", price: "#{rand(0..50)}", category: "Cat 3", store_id: 1)
+Product.create(name: 'Product 4', description: "Description 4", price: "#{rand(0..50)}", category: "Cat 4", store_id: 1)
+Product.create(name: 'Product 5', description: "Description 5", price: "#{rand(0..50)}", category: "Cat 5", store_id: 1)
+Product.create(name: 'Product 6', description: "Description 6", price: "#{rand(0..50)}", category: "Cat 6", store_id: 1)
+Product.create(name: 'Product 7', description: "Description 7", price: "#{rand(0..50)}", category: "Cat 7", store_id: 1)
+Product.create(name: 'Product 8', description: "Description 8", price: "#{rand(0..50)}", category: "Cat 8", store_id: 1)
+Product.create(name: 'Product 9', description: "Description 9", price: "#{rand(0..50)}", category: "Cat 9", store_id: 1)
+Product.create(name: 'Product 10', description: "Description 10", price: "#{rand(0..50)}", category: "Cat 10", store_id: 1)
